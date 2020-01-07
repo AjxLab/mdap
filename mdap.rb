f099334baea6cb2d7fdf3ea42bd429d7e8d6feb8
AjxLab <abe12@mccc.jp>
2020-01-08 00:16:30 +0900

    バーを表示

--- mdap.rb
+++ mdap.rb
@@ -2,7 +2,9 @@
 require 'time'
 
 
-def mdap(n, datetime_format:"%H:%M:%S.%L")
+def mdap(n, datetime_format:"%H:%M:%S.%L",
+         bar_shape:["\e[42m \e[0m", "."]
+        )
   ## -----*----- Progress Bar -----*----- ##
   # 初期時刻
   dt_start = Time.new.strftime(datetime_format)
@@ -13,12 +15,20 @@ def mdap(n, datetime_format:"%H:%M:%S.%L")
     # 現在時刻
     dt_now = Time.new.strftime(datetime_format)
 
-    out_str = "\r|| #{i*100/n}% (#{i}/#{n}) [#{dt_start}-#{dt_now}]"
-    bar = (1..out_str.length).map { |j|
-
-    }
+    # 出力フォーマット
+    out_str = sprintf("[] %d%s (%d/%d) [%s-%s]", i*100/n, '%', i, n, dt_start, dt_now)
+    #out_str = "[] #{i*100/n}% (#{i}/#{n}) [#{dt_start}-#{dt_now}]"
+    bar_len = width-out_str.length
+    bar = (1..bar_len).map { |j|
+      if i/n.to_f >= j/bar_len.to_f
+        bar_shape[0]
+      else
+        bar_shape[1]
+      end
+    }.join
+    print "\r#{out_str.gsub('[]', "[#{bar}]")}"
     yield
   end
 end
 
-mdap(100) { sleep 0.01 }
+mdap(1000) { sleep 0.01 }
