7abdb9ccfc712d7dca5bed608202a6918b997a39
AjxLab <abe12@mccc.jp>
2020-01-08 00:26:05 +0900

    add desc

--- mdap.rb
+++ mdap.rb
@@ -2,22 +2,24 @@
 require 'time'
 
 
-def mdap(n, datetime_format:"%H:%M:%S.%L",
-         bar_shape:["\e[42m \e[0m", "."]
+def mdap(n, desc:nil, bar_shape:["\e[42m \e[0m", "─"],
+         datetime_format:"%H:%M:%S.%L"
         )
   ## -----*----- Progress Bar -----*----- ##
-  # 初期時刻
-  dt_start = Time.new.strftime(datetime_format)
+  dt_start = Time.new
 
   1.upto(n) do |i|
-    # ウィンドウ幅
     width = `tput cols`.to_i
-    # 現在時刻
-    dt_now = Time.new.strftime(datetime_format)
+    dt_now = Time.new
 
     # 出力フォーマット
-    out_str = sprintf("[] %d%s (%d/%d) [%s-%s]", i*100/n, '%', i, n, dt_start, dt_now)
-    #out_str = "[] #{i*100/n}% (#{i}/#{n}) [#{dt_start}-#{dt_now}]"
+    out_str = sprintf("[] %d%s (%d/%d) [%s-%s]", i*100/n, '%', i, n,
+                      dt_start.strftime(datetime_format),
+                      dt_now.strftime(datetime_format)
+                     )
+    out_str = "#{desc}: #{out_str}"  unless desc.nil?
+
+    # 進捗バー
     bar_len = width-out_str.length
     bar = (1..bar_len).map { |j|
       if i/n.to_f >= j/bar_len.to_f
@@ -26,7 +28,10 @@ def mdap(n, datetime_format:"%H:%M:%S.%L",
         bar_shape[1]
       end
     }.join
+
+    # 表示
     print "\r#{out_str.gsub('[]', "[#{bar}]")}"
+
     yield
   end
 end
